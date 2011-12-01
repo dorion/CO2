967fd2903af71865ba5212a67e5c28e91be22048
Your Name <dorionka@freemail.hu>
2011-12-01 18:13:08 +0100

    Create an set contants.

--- CO2.conf.php
+++ CO2.conf.php
@@ -1,2 +1,16 @@
 <?php
+/**
+ * Set the module's constant
+ *
+ */
 
+//CO2 emission constants
+CO2_EMISSION_CAR = 1;
+CO2_EMISSION_TRAIN = 1;
+CO2_EMISSION_AEROPLANE = 1;
+CO2_EMISSION_MCU = 1;
+CO2_EMISSION_VIDCONF_ENDPOINT = 1;
+
+//Vehicel distance limit in km
+DISTANCE_CAR = 300;
+DISTANCE_TRAIN = 500;
