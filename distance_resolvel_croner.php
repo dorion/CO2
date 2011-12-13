166f9d2524872d12272d6fb2a1a373b34c09f85d
Dévai Tamás <dorion@niif.hu>
2011-12-13 11:22:54 +0100

    Improved error handling and documentation.

--- distance_resolvel_croner.php
+++ distance_resolvel_croner.php
@@ -1,5 +1,17 @@
 <?php
 
+/**
+ * Resolve the distance and the duration time with google distance matrix API
+ * http://code.google.com/intl/hu-HU/apis/maps/documentation/distancematrix/
+ *
+ * @param array
+ *  Coordinate of the start point
+ * @param array
+ *  Coordinate of the end point
+ *
+ * @return
+ *  An array if success which include distance and time values and a string if unsuccessfull
+ */
 function distance_and_time_resolver($start_point, $end_point) {
   $lint = curl_init();
 
@@ -25,11 +37,15 @@ function distance_and_time_resolver($start_point, $end_point) {
 
   $result = json_decode($result, TRUE);
 
-  if ($result['rows']['elements'][0]['status'] === 'OK') {
-    return array($distance, $time);
+  if ($result['status'] === 'OK') {
+    if ($result['rows']['elements'][0]['status'] === 'OK') {
+      return array($distance, $time);
+    }
+    else {
+      return $result['rows']['elements'][0]['status'];
+    }
   }
   else {
-    //TODO: make error logic
-    return ();
+    return $result['status'];
   }
 }
