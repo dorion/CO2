18c5445fe90871997c46eb6010e61e4e3f360940
Dévai Tamás <dorion@niif.hu>
2011-12-14 12:43:28 +0100

    Added filter functionality.

--- filter_log.php
+++ filter_log.php
@@ -3,49 +3,45 @@
  * @file Filtered the CDR recordy by varios parameters, then transfer them into the temp table
  */
 
-/**
- * Connected to the CDR database
- */
-function connect_cdr() {
-  $host = 'localhost';
-  $mysql_user = 'CDR';
-  $mysql_passwd = '';
-  $db_name = 'CDR;'
+//DB handling
+require_once('db.inc.php');
 
-  $link = mysql_connect($host, $mysql_user, $mysql_passwd);
-  if (!$link) {
-    die('Could not connect: ' . mysql_error());
-  }
-
-  if(!mysql_select_db($db_name, $link)) {
-    die('Could not chanage database: ' . mysql_error());
-  }
-
-  return $link;
-}
-
-function disconnect_cdr() {
-  mysql_close($link);
-}
-
-function filter() {
-  $db = connect_cdr();
+function filter($id = 0) {
+  $db = connect_sql('cdr');
   $cdr = 'CDR';//table name
   $gk_name = 'NIIF-GK';
   $min_duration = 300;//duration in secound
+  $start_time = '2009-01-01 0:00:00';
 
   $sql = "SELECT
-            ID, duration, connect_time, callerip, calledip
+            ID, confid, connect_time, duration, calling_stationid, callerip, called_stationid, calledip
           FROM ". $cdr ." 
           WHERE
-            gk_name = '". $gk_name ."' AND duration >= ". $min_duration ."";
+            gk_name = '". $gk_name ."' AND duration >= ". $min_duration ." AND ID > ". $id ." AND connect_time >= '". $start_time ."' LIMIT 10000";
   $result = mysql_query($sql, $db);
 
-  disconnect_cdr($db);
+  disconnect_sql($db);
 
   return $result;
 }
 
-function record_inserter($record) {
-  $sql = "INSERT INTO temp_log VALUES ()";
+function record_inserter() {
+  $rows = filter();
+
+  $link = connect_sql('co2');
+  while ($row = mysql_fetch_assoc($rows)) {
+    $sql = "INSERT INTO temp_log".
+            " (conf_id, start_datetime, duration, caller_GDS, called_GDS, caller_IP, called_IP)".
+            " VALUES ('".
+              $row['confid'] ."', '".
+              $row['connect_time']. "', '".
+              $row['duration'] ."', '".
+              $row['calling_stationid'] ."', '".
+              $row['called_stationid']."', '".
+              $row['callerip'] ."', '".
+              $row['calledip'] ."')";
+
+    mysql_query($sql, $link);
+  }
+  disconnect_sql(&$link);
 }
