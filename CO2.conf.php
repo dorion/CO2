30892e63b341553a6fd0a8ef77e472a0b19e59c7
Dévai Tamás <dorion@niif.hu>
2012-01-03 15:58:55 +0100

    Changed some emission values.

--- CO2.conf.php
+++ CO2.conf.php
@@ -5,9 +5,9 @@
  */
 
 //CO2 emission constants
-define('CO2_EMISSION_CAR', 1);
-define('CO2_EMISSION_TRAIN', 1);
-define('CO2_EMISSION_AEROPLANE', 1);
+define('CO2_EMISSION_CAR', 200); //g/km
+define('CO2_EMISSION_TRAIN', 90); //g/km
+define('CO2_EMISSION_AEROPLANE', 500); //g/km
 define('CO2_EMISSION_MCU', 1);
 define('CO2_EMISSION_VIDCONF_ENDPOINT', 1);
 
