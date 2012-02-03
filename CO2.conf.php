1a1c11421bf3f0f3f5f072bbb1507486f045cbd8
Dévai Tamás <dorion@niif.hu>
2012-02-03 11:26:47 +0100

    Added new aeroplane GHG emission constant.

--- CO2.conf.php
+++ CO2.conf.php
@@ -8,10 +8,12 @@
 define('CO2_EMISSION_CAR', 176); //g/km
 define('CO2_EMISSION_TRAIN', 60); //g/km
 define('CO2_EMISSION_AEROPLANE_800', 160); //g/km
-define('CO2_EMISSION_AEROPLANE_800_plus', 100); //g/km
+define('CO2_EMISSION_AEROPLANE_800_PLUS', 100); //g/km
 define('CO2_EMISSION_MCU', 1);
 define('CO2_EMISSION_VIDCONF_ENDPOINT', 1);
 
 //Vehicel distance limit in km
 define('DISTANCE_CAR', 300);
 define('DISTANCE_TRAIN', 500);
+define('DISTANCE_AEROPLANE', 800);
+
