cf8dbb731f15f77313b6fc878d3497fe5bace082
Dévai Tamás <dorion@niif.hu>
2012-03-04 16:37:33 +0100

    Cahnged videoconference constants and rewritten the videoconference emission calculation.

--- CO2.conf.php
+++ CO2.conf.php
@@ -5,12 +5,14 @@
  */
 
 //CO2 emission constants
-define('CO2_EMISSION_CAR', 176); //g/km
-define('CO2_EMISSION_TRAIN', 60); //g/km
-define('CO2_EMISSION_AEROPLANE_800', 160); //g/km
-define('CO2_EMISSION_AEROPLANE_800_PLUS', 100); //g/km
-define('CO2_EMISSION_MCU', 1);
-define('CO2_EMISSION_VIDCONF_ENDPOINT', 1);
+define('CO2_EMISSION_CAR', 176); // g/km
+define('CO2_EMISSION_TRAIN', 60); // g/km
+define('CO2_EMISSION_AEROPLANE_800', 160); // g/km
+define('CO2_EMISSION_AEROPLANE_800_PLUS', 100); // g/km
+define('CO2_EMISSION_MCU', 308); // g/s
+define('CO2_EMISSION_GATEKEEPER', 63); // g/s
+define('CO2_EMISSION_VIDCONF_ENDPOINT', 26); // g/s
+define('CO2_EMISSION_VIDCONF_ENDPOINT_DISPLAY', 42); // g/s
 
 //Vehicel distance limit in km
 define('CO2_DISTANCE_CAR', 300);
